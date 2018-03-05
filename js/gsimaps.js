4785751da3a9442b8379f4aa3de768f370090909
hironoyama <37054420+hironoyama@users.noreply.github.com>
2018-03-05 12:03:04 +0900

    Update gsimaps.js

--- js/gsimaps.js
+++ js/gsimaps.js
@@ -29,8 +29,7 @@ CONFIG.layersTab = [
 			'./layers_txt/layers_topic.txt',
 			'./layers_txt/layers_skhb.txt',
 			'./layers_txt/layers_topic_kusatsushirane.txt',
-			'./layers_txt/layers_topic_nishi.txt',
-			'./layers_txt/sample_layer.txt'
+			'./layers_txt/layers_topic_nishi.txt'
 					]
 	},
 	{
